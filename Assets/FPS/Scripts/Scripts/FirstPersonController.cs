2f73434fda8702ce9fa1f2e0ab0aac2814716d40
The-Fortnite-Guy6-7 <devingokublackballz42907@gmail.com>
2025-11-19 09:23:37 -0800

    Add files via upload

--- Assets/FPS/Scripts/Scripts/FirstPersonController.cs
+++ Assets/FPS/Scripts/Scripts/FirstPersonController.cs
@@ -7,20 +7,20 @@ public class FirstPersonController : MonoBehaviour
 {
     //The camera is inside the player
     public Camera Eyes;
-    
+
     public Rigidbody RB;
     public Projectile3DController ProjectilePrefab;
-    
+
     //Character stats
     public float MouseSensitivity = 3;
     public float WalkSpeed = 10;
     public float JumpPower = 7;
-    
+
     //A list of all the solid objects I'm currently touching
     public List<GameObject> Floors;
 
-    
-    
+
+
     void Start()
     {
         //Turn off my mouse and lock it to center screen
@@ -28,23 +28,23 @@ public class FirstPersonController : MonoBehaviour
         Cursor.visible = false;
     }
 
-    
+
     void Update()
     {
         //If my mouse goes left/right my body moves left/right
         float xRot = Input.GetAxis("Mouse X") * MouseSensitivity;
-        transform.Rotate(0,xRot,0);
-        
+        transform.Rotate(0, xRot, 0);
+
         //If my mouse goes up/down my aim (but not body) go up/down
         float yRot = -Input.GetAxis("Mouse Y") * MouseSensitivity;
-        Eyes.transform.Rotate(yRot,0,0);
+        Eyes.transform.Rotate(yRot, 0, 0);
 
         //Movement code
         if (WalkSpeed > 0)
         {
             //My temp velocity variable
             Vector3 move = Vector3.zero;
-            
+
             //transform.forward/right are relative to the direction my body is facing
             if (Input.GetKey(KeyCode.W))
                 move += transform.forward;
@@ -56,13 +56,13 @@ public class FirstPersonController : MonoBehaviour
                 move += transform.right;
             //I reduce my total movement to 1 and then multiply it by my speed
             move = move.normalized * WalkSpeed;
-            
+
             //If I hit jump and am on the ground, I jump
             if (JumpPower > 0 && Input.GetKeyDown(KeyCode.Space) && OnGround())
                 move.y = JumpPower;
             else  //Otherwise, my Y velocity is whatever it was last frame
                 move.y = RB.linearVelocity.y;
-            
+
             //Plug my calculated velocity into the rigidbody
             RB.linearVelocity = move;
         }
@@ -86,7 +86,7 @@ public class FirstPersonController : MonoBehaviour
     private void OnCollisionEnter(Collision other)
     {
         //If I touch something and it's not already in my list of things I'm touching. . .
-            //Add it to the list
+        //Add it to the list
         if (!Floors.Contains(other.gameObject))
             Floors.Add(other.gameObject);
     }
